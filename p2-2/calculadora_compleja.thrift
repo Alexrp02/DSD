955b59fa94a425cc03352fe22ab12c225f9ba63a
AlexRP <aleeexrp02@gmail.com>
2024-04-03 17:25:06 +0200

    Thrift file for complex calculator

---
+++ p2-2/calculadora_compleja.thrift
@@ -0,0 +1,6 @@
+service CalculadoraCompleja {
+	list<double> sumarVectores(1: list<double> vec1, 2: list<double> vec2)
+	list<double> restarVectores(1: list<double> vec1, 2: list<double> vec2)
+	list<double> productoEscalar(1: list<double> vec1, 2: list<double> vec2)
+	list<double> productoVectorial(1: list<double> vec1, 2: list<double> vec2)
+}
